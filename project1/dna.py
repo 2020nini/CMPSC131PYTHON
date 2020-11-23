f9303ed56387382b3df9cc893932a9a5544558e5
2020nini <wkz5094@psu.edu>
2020-11-23 15:24:25 +0000

    1

--- project1/dna.py
+++ project1/dna.py
@@ -1,6 +1,8 @@
 #Author：Wenrui Zhang wkz5094@psu.edu
+from sys import argv
 import csv
-import sys
+import time
+
 
 def match_seq(seq, crime):
   for s in crime:
@@ -11,32 +13,41 @@ def match_seq(seq, crime):
   return True
 
 
-suspects = {}
-crimes = {}
-result = {}
+def run():
+  suspects = {}
+  crimes = {}
+  result = {}
 
-# with open('crime.csv','r') as f:
-with open(sys.argv[1],'r') as f:
-  crimes = list(csv.DictReader(f))
+  # with open('crime.csv','r') as f:
+  with open(argv[1],'r') as f:
+    crimes = list(csv.DictReader(f))
 
-# with open('suspect.csv','r') as f:
-with open(sys.argv[2], 'r') as f:
-  suspects = list(csv.DictReader(f))
-
-
-
-for s in suspects:
-  for c in crimes:
-    if s['Suspect'] not in result:
-      result[s['Suspect']] = []
-    if match_seq(s['Sequence'], c):
-      result[s['Suspect']].append(c['CrimeID'])
-
-result = [[r,','.join(result[r])] for r in result]
+  # with open('suspect.csv','r') as f:
+  with open(argv[2], 'r') as f:
+    suspects = list(csv.DictReader(f))
 
 
-# with open('res.csv', 'w', newline='') as f:
-with open(sys.argv[3], 'w', newline='') as f:
-  r=csv.writer(f)
-  r.writerow(['Suspect', 'Crimes'])
-  r.writerows(result)
+
+  for s in suspects:
+    for c in crimes:
+      if s['Suspect'] not in result:
+        result[s['Suspect']] = []
+      if match_seq(s['Sequence'], c):
+        result[s['Suspect']].append(c['CrimeID'])
+
+  result = [[r,','.join(result[r])] for r in result]
+
+
+  # with open('res.csv', 'w', newline='') as f:
+  with open(argv[3], 'w', newline='') as f:
+    r=csv.writer(f)
+    r.writerow(['Suspect', 'Crimes'])
+    r.writerows(result)
+
+  return
+
+if __name__ == "__main__":
+  start = time.perf_counter()
+  run()
+  end = time.perf_counter()
+  print(f"Time used: {end-start} seconds")
