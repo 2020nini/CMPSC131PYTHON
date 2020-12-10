1638ce56557f662bd857c74fdd4cdd7250c87a95
2020nini <wkz5094@psu.edu>
2020-12-10 16:13:17 +0000

    1

--- quiz3/quiz3a.py
+++ quiz3/quiz3a.py
@@ -1,15 +1,78 @@
-# Author: Yanling Wang yuw17@psu.edu
+# Author: Wenrui Zhang wkz5094@psu.edu
 # GitHub ID: yanlingpsu
 
-"""
-Your program should print out two lines, each line containing a number
-that is the answer for the quiz question 2 on Canvas.
+import sys
 
-You can assume the words.txt file is in the same directory as your quiz3a.py
-Your program has to show your work of how you get the numerical answers
-on Canvas Quiz.
+def get_data(i,j,lines):
+    total_num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for num in lines[i]:
+        if num in total_num:
+            total_num.remove(num)
+    for index in range(9):
+        if lines[index][j] in total_num:
+            total_num.remove(lines[index][j])
+    arroud_list = []
+    if i in [0,3,6]:
+        i = i + 1
+    elif i in [2,5,8]:
+        i = i - 1
 
-Autograder will run your program as follows and expect 2 numbers in the output:
+    if j in [0, 3, 6]:
+        j = j + 1
+    elif j in [2, 5, 8]:
+        j = j - 1
+    arroud_list = [lines[i][j], lines[i-1][j], lines[i+1][j],
+                       lines[i][j-1], lines[i-1][j-1], lines[i+1][j-1],
+                       lines[i][j+1],lines[i-1][j+1],lines[i+1][j+1]]
+    for num in arroud_list:
+        if num in total_num:
+            total_num.remove(num)
+    return str(total_num)
 
-$python3 quiz3a.py
-"""
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+    with open(file, 'r') as f:
+        lines = f.readlines()
+
+    lines_ = []
+    for line in lines:
+        line = line.strip('\n')
+        print(line)
+
+    for line in lines:
+        line = line.strip('\n')
+        line = line.split(' ')
+        lines_.append(line)
+    row_num = int(sys.argv[2])
+    col_num = int(sys.argv[3])
+
+    for i in range(9):
+        for j in range(9):
+            lines_[i][j] = int(lines_[i][j])
+    print("row %s: %s" % (row_num, lines_[row_num]))
+
+    col_res = []
+    for i in lines_:
+        col_res.append(i[col_num])
+    print("column %s: %s" % (col_num, col_res))
+
+    square = []
+
+    for i in range(3):
+        for j in range(3):
+            square.append(lines_[row_num+i][row_num+j])
+    print("square: %s" % (square))
+
+    if lines_[row_num][col_num] !=0:
+        print("(%s, %s)[%s]" % (row_num, col_num, lines_[row_num][col_num]))
+    else:
+        print("(%s, %s) %s" % (row_num, col_num, get_data(row_num, col_num, lines_)))
+    save_file = sys.argv[4]
+
+    with open(save_file, 'w+') as f:
+        for i in range(9):
+            for j in range(9):
+                if lines_[i][j] == 0:
+                    res = get_data(i, j, lines_)
+                    f.write('%s,%s,"%s"\n' % (i, j, res))
